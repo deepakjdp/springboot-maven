7434563a380d594821cba9adac7c6b272ecb1361
deepakjdp <35351493+deepakjdp@users.noreply.github.com>
2026-05-01 22:43:17 -0400

    Update API URL for user service

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -8,7 +8,7 @@ import { User } from '../models/user.model';
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://localhost:8080/api/users';
+  private apiUrl = 'http://localhost:8090/api/users';
 
   constructor(private http: HttpClient) { }
 
